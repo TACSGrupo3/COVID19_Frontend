ba709e988666594fbebdf343ea88837684e1de15
francoalopez <fran.lopez44@hotmail.com>
2020-06-07 15:09:22 -0300

    Se agrega un proxy config para ambiente local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLDubeVR0yTp8SgqDK0LEa9kaZXlRtiC4",
+    authDomain: "covid19-tacs.firebaseapp.com",
+    databaseURL: "https://covid19-tacs.firebaseio.com",
+    projectId: "covid19-tacs",
+    storageBucket: "covid19-tacs.appspot.com",
+    messagingSenderId: "298727292652",
+    appId: "1:298727292652:web:9db69e71f74fc9c3eb6fc9",
+    measurementId: "G-37TLMRX950"
+  }
 };
