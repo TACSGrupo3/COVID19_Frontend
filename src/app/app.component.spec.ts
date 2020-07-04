ffe273a1867cc8e86442e17559befc89be08b23d
Maximiliano Ricagno <ricagnom@gmail.com>
2020-07-04 19:11:32 -0300

    Avance en manejo de cache de countries

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import {AuthService} from "./services/auth.service";
-import { AngularFirestore } from '@angular/fire/firestore';
+import {AngularFirestore} from '@angular/fire/firestore';
 import {AngularFireModule} from "@angular/fire";
 import {environment} from "../environments/environment";
 import {DatePipe} from "@angular/common";
@@ -20,8 +20,8 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      providers: [AuthService, AngularFirestore,DatePipe],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [AuthService, AngularFirestore, DatePipe],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
@@ -30,17 +30,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  /*it(`should have as title 'Covid19'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Covid19');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Covid19 app is running!');
-  });*/
+  
 });
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,94 +1,105 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { UserModel } from '../model/user.model';
-import { CountriesListModel } from '../model/countriesList.model';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {UserModel} from '../model/user.model';
+import {CountriesListModel} from '../model/countriesList.model';
+import {shareReplay} from 'rxjs/operators';
+import {CountryModel} from "../model/country.model";
+
+const CACHE_SIZE = 1;
 
 @Injectable()
 export class CountriesServices {
-    readonly SERVIDOR_API: string = "/api/";
-    
-    constructor(private http: HttpClient) { }
+  readonly SERVIDOR_API: string = "/api/";
 
-    private getRequest(servicio: string, params: any, queryParams: Array<any>): Observable<any> {
-        var url: string;
-        let paramsString = this.paramsString(params, queryParams);
-        url = this.SERVIDOR_API + servicio + "/" + paramsString;
+  private cache$: Observable<Array<CountryModel>>;
 
-        return this.http.get(url);
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    postRequest(servicio: string, body: any, idToAdd : any) : Observable<any> {
-        var url: string;
-        url = this.SERVIDOR_API + servicio + "/" + idToAdd;
-        let myHeaders: HttpHeaders = new HttpHeaders();
-        let options = { headers : myHeaders };
-        
-        return this.http.post(url, body,options);
-    }
+  private getRequest(servicio: string, params: any, queryParams: Array<any>): Observable<any> {
+    var url: string;
+    let paramsString = this.paramsString(params, queryParams);
+    url = this.SERVIDOR_API + servicio + "/" + paramsString;
 
-    putRequest(servicio: string, body: any, idToModify : any) : Observable<any> {
-        var url: string;
-        url = this.SERVIDOR_API + servicio + "/" + idToModify;
-        let myHeaders: HttpHeaders = new HttpHeaders();
-        let options = { headers : myHeaders };
-        
-        return this.http.put(url, body,options);
-    }
+    return this.http.get(url);
+  }
 
-    deleteRequest(servicio: string, params : any) : Observable<any>{
-        var url: string;
-        let paramsString = this.paramsString(params, null);
-        url = this.SERVIDOR_API + servicio + "/" + paramsString;
+  postRequest(servicio: string, body: any, idToAdd: any): Observable<any> {
+    var url: string;
+    url = this.SERVIDOR_API + servicio + "/" + idToAdd;
+    let myHeaders: HttpHeaders = new HttpHeaders();
+    let options = {headers: myHeaders};
 
-        return this.http.delete(url);
-    }
+    return this.http.post(url, body, options);
+  }
 
-    private paramsString(params: any, queryParams: Array<any>) {
-        if (params == null) params = "";
-        if (queryParams != null) {
-            for (let i = 0; i < queryParams.length; i++) {
-                if (i == 0) {
-                    params = params.toString().concat("?");
-                } else {
-                    params = params.toString().concat("&")
-                }
-                params = params.toString().concat(queryParams[i].toString());
-            }
+  putRequest(servicio: string, body: any, idToModify: any): Observable<any> {
+    var url: string;
+    url = this.SERVIDOR_API + servicio + "/" + idToModify;
+    let myHeaders: HttpHeaders = new HttpHeaders();
+    let options = {headers: myHeaders};
+
+    return this.http.put(url, body, options);
+  }
+
+  deleteRequest(servicio: string, params: any): Observable<any> {
+    var url: string;
+    let paramsString = this.paramsString(params, null);
+    url = this.SERVIDOR_API + servicio + "/" + paramsString;
+
+    return this.http.delete(url);
+  }
+
+  private paramsString(params: any, queryParams: Array<any>) {
+    if (params == null) params = "";
+    if (queryParams != null) {
+      for (let i = 0; i < queryParams.length; i++) {
+        if (i == 0) {
+          params = params.toString().concat("?");
+        } else {
+          params = params.toString().concat("&")
         }
-        return params;
+        params = params.toString().concat(queryParams[i].toString());
+      }
     }
+    return params;
+  }
 
-    findAll() {
-        return this.getRequest("countries", null, null);
+  findAll() {
+    if (!this.cache$) {
+      this.cache$ = this.getRequest("countries", null, null).pipe(
+        shareReplay(CACHE_SIZE));
     }
+    return this.cache$;
+  }
 
-    findNearCountries(latitud : any, longitud : any, maxCant : any){
-        return this.getRequest("countries", null, ["latitude="+latitud,"longitude="+longitud, "maxCountries="+maxCant]);
-    }
+  findNearCountries(latitud: any, longitud: any, maxCant: any) {
+    return this.getRequest("countries", null, ["latitude=" + latitud, "longitude=" + longitud, "maxCountries=" + maxCant]);
+  }
 
-    getCountriesList(){
-        return this.getRequest("countriesList",null,null);
-    }
+  getCountriesList() {
+    return this.getRequest("countriesList", null, null);
+  }
 
-    getCountriesListById(listId :number){
-        return this.getRequest("countriesList",listId,null);
-    }
+  getCountriesListById(listId: number) {
+    return this.getRequest("countriesList", listId, null);
+  }
 
-    getCountriesListOfUser(user : UserModel){
-        return this.getRequest("users", user.id+"/countriesList", null);
-    }
+  getCountriesListOfUser(user: UserModel) {
+    return this.getRequest("users", user.id + "/countriesList", null);
+  }
 
-    addCountriesList(userId : String, countryList : Array<CountriesListModel>){
-        return this.postRequest("users", countryList, userId + "/countriesList");
-    }
+  addCountriesList(userId: String, countryList: Array<CountriesListModel>) {
+    return this.postRequest("users", countryList, userId + "/countriesList");
+  }
 
-    modifyCountriesList(listToModify: CountriesListModel){
-        return this.putRequest("countriesList",listToModify, listToModify.id);
-    }
+  modifyCountriesList(listToModify: CountriesListModel) {
+    return this.putRequest("countriesList", listToModify, listToModify.id);
+  }
 
-    deleteCountriesList(countriesListId : number){
-        return this.deleteRequest("countriesList", countriesListId);
-    }
+  deleteCountriesList(countriesListId: number) {
+    return this.deleteRequest("countriesList", countriesListId);
+  }
 }
 
